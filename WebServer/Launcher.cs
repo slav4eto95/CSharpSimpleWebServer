1ba9bb9d69ee393d78a3278a85ec3f47bf20fc23
Slavena Koleva <slavena_kolevaa@abv.bg>
2018-12-26 18:45:49 +0200

    change

--- WebServer/Launcher.cs
+++ WebServer/Launcher.cs
@@ -20,7 +20,7 @@ namespace MyCoolWebServer
             IAppRouteConfig routeConfig = new AppRouteConfig();
             application.Configure(routeConfig);
 
-            var webServer = new WebServer(1337, routeConfig);
+            var webServer = new WebServer(1234, routeConfig);
             webServer.Run();
         }
     }
